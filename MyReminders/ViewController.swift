03267a81d1d58485d45c339b5d67c43896a5eb59
Melike <melikebolen1@gmail.com>
2022-06-16 13:42:31 +0300

    Initial Commit

---
+++ MyReminders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyReminders
+//
+//  Created by Melike Bolen on 6/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
